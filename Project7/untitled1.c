cbf6675605cf8d07e64a461b2ccdd3b0c11d6b10
mojisleep <mojisleep@gmail.com>
2024-09-03 21:14:04 +0800

    4

---
+++ Project7/untitled1.c
@@ -0,0 +1,24 @@
+//#include<stdio.h>
+//#include<stdbool.h>
+//int main()
+//{
+//	bool b=6>5;
+//	bool t=true;
+//	t=3;
+//	printf("%d\n",b);
+//	printf("%d\n",t);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a=-1;
+//	if(a>0&&a++>1){
+//		printf("ok\n");
+//	}
+//	
+//	printf("%d\n",a);
+//	return 0;
+//}
+
