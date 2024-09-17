0bbe818ab0e1bf1c798f616b612371e119af6314
mojisleep <mojisleep@gmail.com>
2024-09-17 17:50:50 +0800

    7

--- Project10/untitled2.c
+++ Project10/untitled2.c
@@ -70,31 +70,46 @@
 //	return 0;
 //} 
 
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<time.h>
+//int main()
+//{
+//	srand(time(0));
+//	int number=rand()%100+1;
+//	int count=0;
+//	int a=0;
+//	printf("我已经想到一个1到100之间的数。");
+//	do{
+//	printf("猜猜这个1到100之间的数：");		
+//	scanf("%d",&a);
+//	count++;
+//	if(a>number)
+//	{
+//		printf("你猜大了");
+//	}
+//	else if(a<number)
+//	{
+//		printf("你猜小了");
+//	}
+//	}while(a!=number);
+//	printf("你用%d次猜到答案。\n",count);
+//	return 0;
+//}
+
 #include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
 int main()
 {
-	srand(time(0));
-	int number=rand()%100+1;
-	int count=0;
-	int a=0;
-	printf("我已经想到一个1到100之间的数。");
-	do{
-	printf("猜猜这个1到100之间的数：");		
-	scanf("%d",&a);
-	count++;
-	if(a>number)
+	int x;
+	scanf("%d",&x);
+	int digit;
+	int ret=0;
+	while(x>0)
 	{
-		printf("你猜大了");
+		digit=x%10;
+		ret=ret*10+digit;
+		x/=10;
 	}
-	else if(a<number)
-	{
-		printf("你猜小了");
-	}
-	}while(a!=number);
-	printf("你用%d次猜到答案。\n",count);
+	printf("%d",ret);
 	return 0;
 }
-
-
